23bc08e694eb882c4f887b0b05ababed8d9c1cfe
kuokyn <viktoria2nikiforova@gmail.com>
2022-09-26 21:10:28 +0300

    pract_3 start

---
+++ public_html/index.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+    <title>Hello world page</title>
+    <link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+<body>
+<h1>Таблица пользователей данного продукта</h1>
+<table>
+    <tr><th>Id</th><th>Name</th><th>Surname</th></tr>
+    <?php
+    $mysqli = new mysqli("mysqldb", "root", "root", "mysqldatabase");
+    $result = $mysqli->query("SELECT * FROM users");
+    foreach ($result as $row){
+        echo "<tr><td>{$row['id']}</td><td>{$row['name']}</td><td>{$row['surname']}</td></tr>";
+    }
+    ?>
+</table>
+<?php
+phpinfo();
+?>
+</body>
+</html>
